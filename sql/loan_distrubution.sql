132a63145cbf9dde6f323178e527f18f7c777c28
OlhaPia <olhapiatenko@gmail.com>
2025-02-10 17:15:45 -0500

    Add sql queries

---
+++ sql/loan_distrubution.sql
@@ -0,0 +1,8 @@
+# Loan Distribution by Country
+
+SELECT 
+    "Country / Economy", 
+    SUM("Disbursed Amount (US$)") AS total_disbursed
+FROM banking_info
+GROUP BY "Country / Economy"
+ORDER BY total_disbursed DESC;
